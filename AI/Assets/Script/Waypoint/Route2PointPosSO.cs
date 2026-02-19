688a37489d1ff475f32584f7bef51e61c25c1acf
Dexxo <29daffa03@gmail.com>
2026-02-19 15:00:59 +0700

    AI

---
+++ AI/Assets/Script/Waypoint/Route2PointPosSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Waypoint/Route 2 Point (Position)", menuName = "Route2PointPos")]
+public class Route2PointPosSO : ScriptableObject
+{
+    public Vector3 pointA;
+    public Vector3 pointB;
+}
